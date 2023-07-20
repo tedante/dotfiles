ca98f5abaf6176818f798fac5a4b5c1ece368935
Teddy <teddykoerniadi@gmail.com>
2023-07-20 10:44:55 +0700

    add new templates

--- templates/cjs-app.js
+++ templates/cjs-app.js
@@ -1,11 +1,14 @@
 const express = require('express')
+const routes = require('./routes/index.js');
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use("/", routes)
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on http://localhost:${port}`)
 })
