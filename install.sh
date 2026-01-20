c03d922db210590cd068962b11dbe37d5f542888
Teddy K <teddykoerniadi@gmail.com>
2026-01-20 17:34:00 +0700

    update

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Define colors for output
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+echo -e "${BLUE}>>> Downloading Teddy's Dotfiles...${NC}"
+
+# 1. Clone the repository into a temporary location or ~/dotfiles
+if [ -d "$HOME/dotfiles" ]; then
+    echo "Directory ~/dotfiles already exists. Updating..."
+    cd "$HOME/dotfiles" && git pull
+else
+    git clone https://github.com/tedante/dotfiles.git "$HOME/dotfiles"
+fi
+
+# 2. Change directory
+cd "$HOME/dotfiles"
+
+# 3. Make scripts executable
+chmod +x setup-mint.sh setup-keymap.sh
+
+# 4. Execute the master setup script
+./setup-mint.sh
