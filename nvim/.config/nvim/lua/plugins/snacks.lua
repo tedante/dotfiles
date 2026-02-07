3c5569822461385e8fc772f12947042ae36ecf40
Teddy K <teddykoerniadi@gmail.com>
2026-02-07 12:23:17 +0700

    configuration files for LazyVim setup

---
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        hidden = true,
+        ignored = true,
+        sources = {
+          files = {
+            hidden = true, -- Show hidden/dotfiles
+          },
+        },
+      },
+    },
+  },
+}
